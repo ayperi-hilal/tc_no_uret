c06e0666128e79c24732e21f9b404528d9758058
hilal <62089994+ayperi-hilal@users.noreply.github.com>
2022-03-09 15:09:02 +0300

    Add files via upload

---
+++ kimlikNoUret/kimlikNoUret/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace kimlikNoUret
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_uret = new DevExpress.XtraEditors.SimpleButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_uret
+            // 
+            this.btn_uret.ImageOptions.Image = global::kimlikNoUret.Properties.Resources.version_16x16;
+            this.btn_uret.Location = new System.Drawing.Point(166, 78);
+            this.btn_uret.Name = "btn_uret";
+            this.btn_uret.Size = new System.Drawing.Size(90, 23);
+            this.btn_uret.TabIndex = 0;
+            this.btn_uret.Text = "TC NO ÜRET";
+            this.btn_uret.Click += new System.EventHandler(this.btn_uret_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(288, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(288, 152);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(170, 238);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_uret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btn_uret;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ kimlikNoUret/kimlikNoUret/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kimlikNoUret
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_uret_Click(object sender, EventArgs e)
+        {
+            temizle();
+            int[] rakamlar = new int[11];
+
+            Random ran= new Random();
+
+            for (int i = 0; i < 9; i++)
+            {
+                rakamlar[i] = ran.Next(0, 9);
+            }
+
+            if (rakamlar[0] == 0)
+            {
+                rakamlar[0] = ran.Next(0, 9);
+            }
+            else
+            {
+                int onuncuhane = ((rakamlar[0] + rakamlar[2] + rakamlar[4] + rakamlar[6] + rakamlar[8]) * 7) - (rakamlar[1] + rakamlar[3] + rakamlar[5] + rakamlar[7]);
+                int ondanbolum = onuncuhane % 10;
+                rakamlar[9] = ondanbolum;
+                int onbirincihane = rakamlar[0] + rakamlar[1] + rakamlar[2] + rakamlar[3] + rakamlar[4] + rakamlar[5] + rakamlar[6] + rakamlar[7] + rakamlar[8] + rakamlar[9];
+                int modu = onbirincihane % 10;
+                
+                rakamlar[10] = modu;
+            }
+            foreach (int a in rakamlar)
+            {
+                listBox1.Items.Add(a);
+                textBox1.Text += a.ToString();
+            }
+
+
+
+        }
+
+        public void temizle()
+        {
+            textBox1.Text = "";
+            listBox1.Items.Clear();
+        }
+    }
+}
